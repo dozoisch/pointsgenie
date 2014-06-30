525902fc05d29f2c469624057a97347182b67a98
dozoisch <hugo@dozoisch.com>
2014-06-30 14:02:09 -0400

    better handling for promocard

--- src/client/pages/user.jsx
+++ src/client/pages/user.jsx
@@ -4,6 +4,40 @@ var React = require('react');
 
 module.exports = React.createClass({
   render: function () {
+    var self = this;
+    var promocard = (function () {
+      var inner = (function () {
+         if(self.props.user && self.props.user.promocard && self.props.user.promocard.price) {
+          return (
+            <form className="form-horizontal">
+              <fieldset>
+                <div className="form-group">
+                  <label className="control-label col-md-3">Prix Payé:</label>
+                  <div className="col-md-6 col-md-offset-0">
+                    <input disabled className="form-control" type="text" value="{'this.props.user.promocard.price'}" />
+                  </div>
+                </div>
+                <div className="form-group">
+                  <label className="control-label col-md-3">Date:</label>
+                  <div className="col-md-6 col-md-offset-0">
+                    <input disabled className="form-control" type="text" value="{'this.props.promocard.created'}" />
+                  </div>
+                </div>
+              </fieldset>
+            </form>
+          );
+        } else {
+          return (<p> La promocarte n'a pas été achetée encore</p>);
+        }
+      })();
+      return (
+        <div className="user-promocard-info">
+          <h4>Promocarte</h4>
+          {inner}
+        </div>
+      );
+    })();
+
     return (
       <div className="user-info">
         <div className="user-general-info">
@@ -11,11 +45,11 @@ module.exports = React.createClass({
           <form className="form-horizontal">
             <fieldset>
             <div className="form-group">
-              <label className="control-label col-md-3">cip:</label>
+              <label className="control-label col-md-3">Cip:</label>
               <div className="col-md-6 col-md-offset-0"><input disabled className="form-control" type="text" value="{'this.props.user.cip'}" /></div>
             </div>
             <div className="form-group">
-              <label className="control-label col-md-3">Email:</label>
+              <label className="control-label col-md-3">Courriel:</label>
               <div className="col-md-6 col-md-offset-0"><input disabled className="form-control" type="text" value="{'this.props.user.email'}" /></div>
             </div>
             <div className="form-group">
@@ -30,32 +64,28 @@ module.exports = React.createClass({
           <form className="form-horizontal" role="form" name="password-change" action="" method="POST">
             <fieldset>
             <div className="form-group">
-              <label className="control-label col-md-3" htmlFor="old_pw">mot de passe courant:</label>
-              <div className="col-md-6 col-md-offset-0"><input className="form-control" type="password" name="old_pw" /></div>
+              <label className="control-label col-md-3" htmlFor="old_pw">Mot de passe courant:</label>
+              <div className="col-md-6 col-md-offset-0">
+                <input className="form-control" type="password" name="old_pw" placeholder="ancien"/>
+              </div>
             </div>
             <div className="form-group">
-              <label className="control-label col-md-3" htmlFor="new_pw1">nouveau mot de passe:</label>
-              <div className="col-md-6 col-md-offset-0"><input className="form-control" type="password" name="new_pw1" /></div>
+              <label className="control-label col-md-3" htmlFor="new_pw1">Nouveau mot de passe:</label>
+              <div className="col-md-6 col-md-offset-0">
+                <input className="form-control" type="password" name="new_pw1" placeholder="nouveau" />
+              </div>
             </div>
             <div className="form-group">
-              <label className="control-label col-md-3" htmlFor="new_pw2">répéter mot de passe:</label>
-              <div className="col-md-6 col-md-offset-0"><input className="form-control" type="password" name="new_pw2" /></div>
+              <label className="control-label col-md-3" htmlFor="new_pw2">Répéter mot de passe:</label>
+              <div className="col-md-6 col-md-offset-0">
+                <input className="form-control" type="password" name="new_pw2" placeholder="répéter" />
+              </div>
             </div>
             <button type="submit" className="btn btn-success">Changer mot de passe</button>
             </fieldset>
           </form>
         </div>
-        <div className="user-promocard-info">
-          <h4>Promocarte</h4>
-          <form className="form-horizontal">
-            <fieldset>
-            <div className="form-group">
-              <label className="control-label col-md-3">A acheté la carte: <input disabled type="checkbox" data-props="this.props.user.promocard.price ? 'checked=true': ''" /></label>
-
-            </div>
-            </fieldset>
-          </form>
-        </div>
+        {promocard}
       </div>
     );
   }
