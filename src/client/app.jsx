d329d2db32d37d0e7a0c44ff98b22fad3c49bca5
dozoisch <hugo@dozoisch.com>
2014-07-24 00:08:31 -0400

    removed null page

--- src/client/app.jsx
+++ src/client/app.jsx
@@ -7,7 +7,6 @@ var Link = reactNestedRouter.Link;
 
 var IndexPage = require("./pages/index");
 var ProfilePage = require("./pages/profile");
-var NullPage = require("./pages/null");
 
 var container = document.getElementById("page-container");
 
@@ -21,7 +20,6 @@ var App = React.createClass({
           <ul className="nav nav-pills nav-stacked" >
             <li><Link to="index">Accueil</Link></li>
             <li><Link to="profile">Profil</Link></li>
-            <li><Link to="null-page">Null</Link></li>
           </ul>
         </nav>
         <div className="col-md-10 well">
@@ -36,7 +34,6 @@ React.renderComponent(
   <Route handler={App} >
     <Route name="index" path="/" handler={IndexPage} />
     <Route name="profile" path="profile" handler={ProfilePage} />
-    <Route name="null-page" path="null" handler={NullPage} />
   </Route>
 , container);
 
