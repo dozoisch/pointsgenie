1cedffa2e6f835f6278359c4776a20a89743e869
dozoisch <hugo@dozoisch.com>
2014-08-28 14:15:09 -0400

    Added error message when login fails

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -1,5 +1,9 @@
 exports.login = function *() {
-  this.body = yield this.render("auth");
+  var args = {};
+  if (this.query.error) {
+    args.error = "Le cip ou le mot de passe est incorrect.";
+  }
+  this.body = yield this.render("auth", args);
 };
 
 exports.logout = function *() {
